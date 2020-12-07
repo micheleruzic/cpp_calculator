c1fe3e700ea328cd52db1eb3d1c2d631e7607498
Michele Ruzic <michele.ruzic@me.com>
2020-12-07 19:03:26 +0100

    Added cout for 1st number, 2nd number and operation Added the modulo operation

--- main.cpp
+++ main.cpp
@@ -5,8 +5,11 @@ int main() {
     cout << "Welcome to C++ calculator" << endl;
     int a = 0, b = 0, c = 0;
     char opr;
+    cout << "Enter a first number: ";
     cin >> a;
+    cout << "Enter the operation which you would like to perform: ";
     cin >> opr;
+    cout << "Enter the second number: ";
     cin >> b;
     if (opr == '+'){
         c = a + b;
@@ -22,6 +25,10 @@ int main() {
     }
     else if (opr == '/'){
         c = a / b;
+        cout << c;
+    }
+    else if (opr == '%'){
+        c = a % b;
         cout << c;
     }
     else {
